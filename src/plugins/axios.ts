39a0d8e00bf39cb50c8dc151536d19cc46f6c5d9
henry-codex <henryteimensah@gmail.com>
2026-01-29 13:13:04 +0000

    backend config

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -6,12 +6,14 @@ const BACKEND_CONFIG = {
   // Set to 'go' or 'laravel' to switch backends
   BACKEND_TYPE: 'go', // Change this to 'laravel' to use Laravel backend
   
-  // Go backend URLs - switch between local, ngrok, and Heroku
+  // Go backend URLs - switch between local, ngrok, Heroku, and DigitalOcean
   GO_BACKEND_URL_LOCAL: 'http://localhost:8080',
   GO_BACKEND_URL_NGROK: 'https://8f5e6659a95f.ngrok-free.app',
   GO_BACKEND_URL_HEROKU: 'https://gcx-backend-api-e4d0fabe07d7.herokuapp.com',
-  GO_USE_NGROK: false, // Set to true for ngrok, false for local
-  GO_USE_HEROKU: true, // Set to true for Heroku, false for local/ngrok
+  GO_BACKEND_URL_DIGITALOCEAN: 'http://188.166.159.42:8081',
+  GO_USE_NGROK: false, // Set to true for ngrok
+  GO_USE_HEROKU: false, // Set to true for Heroku
+  GO_USE_DIGITALOCEAN: true, // Set to true for DigitalOcean
   
   LARAVEL_BACKEND_URL: 'http://localhost:8000'
 }
@@ -19,6 +21,9 @@ const BACKEND_CONFIG = {
 // Get the appropriate backend URL
 const getBackendURL = () => {
   if (BACKEND_CONFIG.BACKEND_TYPE === 'go') {
+    if (BACKEND_CONFIG.GO_USE_DIGITALOCEAN) {
+      return BACKEND_CONFIG.GO_BACKEND_URL_DIGITALOCEAN
+    }
     if (BACKEND_CONFIG.GO_USE_HEROKU) {
       return BACKEND_CONFIG.GO_BACKEND_URL_HEROKU
     }
